bd28cf6c29f8476a533f14f19201e39fbc809f9e
indiev <ehnawh@gmail.com>
2020-03-29 20:01:01 +0900

    Added translation

---
+++ src/Translations/languages/en.ts
@@ -0,0 +1,26 @@
+export default {
+  common: {},
+  header: {
+    logo: 'Logo',
+    signUp: 'Sign Up',
+    signDown: 'Sign Down',
+    signIn: 'Sign In',
+    signOut: 'Sign Out'
+  },
+  main: {
+    title: 'Main'
+  },
+  signUp: {
+    title: 'Sign Up'
+  },
+  signDown: {
+    title: 'Sign Down'
+  },
+  signIn: {
+    title: 'Sign In',
+    confirmButton: 'Sign In'
+  },
+  signOut: {
+    title: 'Sign Out'
+  }
+};
