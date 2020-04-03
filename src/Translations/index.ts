65588f5c54e25378efad5339c8471f86d4a931fd
indiev <ehnawh@gmail.com>
2020-04-04 00:30:03 +0900

    turn off debug of translation

--- src/Translations/index.ts
+++ src/Translations/index.ts
@@ -10,7 +10,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    debug: true,
+    // debug: true,
     resources,
     lng: 'en',
     fallbackLng: 'en',
